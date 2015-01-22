227a146f5aa4f34c6f3baa697ad6c65c4248eaa5
Florent Biville <florent.biville@gmail.com>
2015-01-22 11:48:40 +0100

    Add basic style and fake search

--- vidal-search.js
+++ vidal-search.js
@@ -1,1 +1,11 @@
-alert('Vidal Search');
+var 	logoUrl = chrome.extension.getURL('logo_vidal.png'),
+    	search = function() {
+		alert('Calling APIs');
+	};
+document.querySelector('body').insertAdjacentHTML(
+	'beforeend', 
+	'<input id="vidal-search" type="image" src="' + logoUrl + '" alt="Vidal Search"  />'
+);
+
+document.querySelector('#vidal-search')
+	.addEventListener('click', search);
