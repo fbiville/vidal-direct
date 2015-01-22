cdb0f5029cf567a138830f0d32efb44ebdfea6f3
@fbiville <florent.biville@gmail.com>
2015-01-22 14:55:03 +0100

    Parse molecule API into structured objects

--- vidal-search.js
+++ vidal-search.js
@@ -1,13 +1,33 @@
 var 	logoUrl = chrome.extension.getURL('logo_vidal.png'),
     	search = function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', 'http://api.vidal.fr/rest/api/products?page-size=1000', true);
-		xhr.onreadystatechange = function() {
-  			if (xhr.readyState == 4) {
-    				console.log(xhr.responseText);
- 			}
-		}
-		xhr.send();
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'http://api.vidal.fr/rest/api/molecules?startwith=a&page-size=1000', true);
+			xhr.onreadystatechange = function() {
+	  			if (xhr.readyState == 4) {
+					var results = [];
+	  				var $xml = $($.parseXML(xhr.responseText));
+	    				var all = $xml.find('entry id, entry title');
+					$.each(all, function(i,el) {
+						var $el = $(el);
+						if (i % 2 == 0) {
+							results.push({'title': $el.text()});
+						}
+						else {
+							var prev = results[Math.floor(i/2)]['id'] = replaceVidalId($el.text());
+						}
+						if (i == all.length - 1) {
+							highlight(results);
+						}
+					});
+	 			}
+			}
+			xhr.send();
+		},
+	replaceVidalId = function(text) { // terrible hack :)
+		return text.match(/\d+/)[0];
+	},
+	highlight = function(results) {
+		console.log(results);
 	};
 
 document.querySelector('body').insertAdjacentHTML(
